a1140444c6c36fc4700f090c065a5b97243343da
윤민 김 <dbsalszz@naver.com>
2020-10-11 18:07:40 +0900

    김윤민_한컴타자연습

--- WindowsFormsApp1008/Form1.Designer.cs
+++ WindowsFormsApp1008/Form1.Designer.cs
@@ -38,40 +38,50 @@
             this.button2 = new System.Windows.Forms.Button();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.groupBox1.ForeColor = System.Drawing.Color.Green;
-            this.groupBox1.Location = new System.Drawing.Point(81, 86);
+            this.groupBox1.Location = new System.Drawing.Point(93, 108);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox1.Size = new System.Drawing.Size(229, 125);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "점수";
             // 
             // label1
             // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(46, 43);
+            this.label1.Location = new System.Drawing.Point(53, 54);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 12);
+            this.label1.Size = new System.Drawing.Size(77, 15);
             this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Text = "처음 점수";
             // 
             // groupBox2
             // 
+            this.groupBox2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.groupBox2.Controls.Add(this.label2);
             this.groupBox2.Font = new System.Drawing.Font("굴림", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.groupBox2.ForeColor = System.Drawing.Color.Green;
-            this.groupBox2.Location = new System.Drawing.Point(425, 97);
+            this.groupBox2.Location = new System.Drawing.Point(504, 108);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.groupBox2.Size = new System.Drawing.Size(229, 125);
             this.groupBox2.TabIndex = 1;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "남은 기회";
@@ -79,25 +89,28 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(46, 43);
+            this.label2.Location = new System.Drawing.Point(53, 54);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(87, 12);
+            this.label2.Size = new System.Drawing.Size(77, 15);
             this.label2.TabIndex = 1;
-            this.label2.Text = "label2      회";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.label2.Text = "처음 기회";
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(101, 236);
+            this.progressBar1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.progressBar1.Location = new System.Drawing.Point(93, 295);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(541, 23);
+            this.progressBar1.Size = new System.Drawing.Size(640, 29);
             this.progressBar1.TabIndex = 2;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(724, 192);
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.button1.Location = new System.Drawing.Point(827, 108);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(86, 29);
             this.button1.TabIndex = 3;
             this.button1.Text = "On";
             this.button1.UseVisualStyleBackColor = true;
@@ -105,9 +118,11 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(724, 236);
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Right;
+            this.button2.Location = new System.Drawing.Point(827, 204);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(86, 29);
             this.button2.TabIndex = 4;
             this.button2.Text = "Off";
             this.button2.UseVisualStyleBackColor = true;
@@ -115,42 +130,80 @@
             // 
             // timer1
             // 
-            this.timer1.Interval = 500;
+            this.timer1.Interval = 3000;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // textBox1
             // 
+            this.textBox1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.textBox1.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.textBox1.Location = new System.Drawing.Point(101, 341);
+            this.textBox1.Location = new System.Drawing.Point(93, 426);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(541, 32);
+            this.textBox1.Size = new System.Drawing.Size(640, 38);
             this.textBox1.TabIndex = 5;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
             // 
-            // label3
+            // label4
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("굴림", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label3.Location = new System.Drawing.Point(328, 294);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(64, 21);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "label3";
-            this.label3.Click += new System.EventHandler(this.label3_Click);
+            this.label4.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("굴림", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label4.Location = new System.Drawing.Point(90, 378);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 24);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "문제 생성";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(94, 36);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(92, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "난이도 하";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(504, 36);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(92, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "난이도 상";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button5.Location = new System.Drawing.Point(311, 36);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(92, 23);
+            this.button5.TabIndex = 10;
+            this.button5.Text = "난이도 중";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // 타자연습
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label3);
+            this.ClientSize = new System.Drawing.Size(914, 562);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.groupBox2);
             this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "타자연습";
             this.Text = "한컴타자연습";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -174,7 +227,10 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- WindowsFormsApp1008/Form1.cs
+++ WindowsFormsApp1008/Form1.cs
@@ -3,37 +3,57 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp1008
 {
-
     public partial class 타자연습 : Form
     {
+        Random rand = new Random();
         List<string> sentence = new List<string>();
-       
+        int count = 10;
+        int initialscore = 0;
+
+
         public 타자연습()
         {
             InitializeComponent();
+            StreamReader rs = new StreamReader("별 헤는 밤-윤동주.txt", Encoding.UTF8);
+
+            while (!rs.EndOfStream)
+            {
+                sentence.Add(rs.ReadLine());
+            }
+        }
+
+        private void Form1_Load()       //아래서 게임을 재시작 시 초기화 때 쓰기 위해서 오버로딩
+        {
+            count = 10;
+            initialscore = 0;
+            label1.Text = initialscore.ToString();
+            label2.Text = $"{count}회";
+            label4.Text = sentence[rand.Next(0, sentence.Count)];
+            progressBar1.Value = 100;
+            timer1.Stop();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            label1.Text = "200";   //속성에 값을 준다 이러면 셋에 간다
-            progressBar1.Value = 100;
-            sentence.Add("무궁화 꽃이 피었습니다.");
-            sentence.Add("진달래 꽃.");
-            sentence.Add(">A<");
-            sentence.Add("무궁화 꽃이 피었습니다.");
+            label1.Text = initialscore.ToString();   //속성에 값을 준다 이러면 셋에 간다
+            label2.Text = $"{count}회";
+            label4.Text = sentence[rand.Next(0, sentence.Count)];
 
+            progressBar1.Value = 100;
         }
 
         private void label2_Click(object sender, EventArgs e)
         {
-            label1.Text = "회";   //속성에 값을 준다 이러면 셋에 간다
+
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -49,10 +69,11 @@ namespace WindowsFormsApp1008
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            if (progressBar1.Value < 10)
+            if (progressBar1.Value < 10 )
             {
-                progressBar1.Value = 0;
+
                 timer1.Stop();
+                Restart();
             }
             else
                 progressBar1.Value = progressBar1.Value - 10;
@@ -61,8 +82,10 @@ namespace WindowsFormsApp1008
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-
-            timer1.Start();
+            if (progressBar1.Value > 0)
+            {
+                timer1.Start();
+            }
         }
 
         private void textBox1_KeyDown(object sender, KeyEventArgs e)
@@ -71,28 +94,70 @@ namespace WindowsFormsApp1008
 
         }
 
-        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)      //아스키코드로 된 것만 취급, 단순 키 입력일 때 사용
         {
-            //아스키코드로 된 것만 취급, 단순 키 입력일 때 사용
-            if (e.KeyChar == 13)
+            
+            if (e.KeyChar == 13)        //정답체크     //반드시 엔터를 눌렀을 때에만 답을 체크한다
             {
-
                 timer1.Stop();
-                //정답체크
-                //if(label3.Text == textBox1.Text)
-                //점수 올리기
-                //else 
-                //기회를 빼준다 문제를 다시 출제한다
+                if (label4.Text == textBox1.Text)       //점수 올리기
+                {
+                    label1.Text = (initialscore += 100).ToString();
+                    label4.Text = sentence[rand.Next(0, sentence.Count)];       //다음 문제로 넘어가야지
+                    textBox1.Clear();
+                    if (progressBar1.Value < 100)
+                        progressBar1.Value += 10;
+                }
 
+                else
+                {
+                        //오답
+                }
+                {
+                    textBox1.Clear();
+                    label2.Text = $"{--count}회";
+                }
             }
+            if (count == 0)     //기회 소진시에는 다시 할 건지 물어봐야지
+            {
+                Restart();
+            }
+        }
+
+        private void Restart()      //다시 할 건지 의사 물어보기
+        {
+            timer1.Stop();
+            MessageBox.Show("짱 못하시네요 ㅎㅎ", "Game Over");
+            DialogResult result = MessageBox.Show("다시 시작하시겠습니까?", "Game Over", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                textBox1.Clear();
+                this.Form1_Load();
+            }
+            else if (result == DialogResult.No)
+                Environment.Exit(001);
         }
 
         private void label3_Click(object sender, EventArgs e)
         {
-            Random rand = new Random();
-            int num = rand.Next(0, 5);
 
-            label3.Text = sentence[num].ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)      //난이도 하
+        {
+            timer1.Interval = 3000;
+        }
+
+        private void button5_Click(object sender, EventArgs e)      //난이도 중
+        {
+            timer1.Interval = 1500;
+        }
+
+        private void button4_Click(object sender, EventArgs e)      //난이도 상
+        {
+            timer1.Interval = 1000;
         }
     }
 }
